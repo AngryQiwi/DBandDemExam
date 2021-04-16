7696badb6dbb03a57092aed9ae270dcebf015ce3
StpnPntr <stpnpntr@gmail.com>
2021-04-16 10:48:57 +0300

    Dada

--- src/main/java/AddEditForm.java
+++ src/main/java/AddEditForm.java
@@ -1,6 +1,7 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
@@ -9,8 +10,9 @@ import java.util.ResourceBundle;
 public class AddEditForm{
     @FXML
     private TextField add_edit_field;
-@FXML
-    public void initialize(ActionEvent actionEvent) {
+    @FXML
+    private javafx.scene.control.Button Button;
+    public void button(ActionEvent actionEvent) {
         add_edit_field.setText(ViewDBForm.getEditableTest().f_name);
     }
 }
